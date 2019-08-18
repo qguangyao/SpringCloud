f9d6e343f95d2b9855f6d03b7173101d098a6852
qguangayo <1018399941@qq.com>
2019-08-19 01:27:39 +0800

    database test

--- microservicecloud-12-product-config-8003/src/main/java/com/qt/ProductProvider_Config_8003.java
+++ microservicecloud-12-product-config-8003/src/main/java/com/qt/ProductProvider_Config_8003.java
@@ -1,11 +1,13 @@
 package com.qt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.qt.mapper")
 public class ProductProvider_Config_8003 {
     public static void main(String[] args) {
         SpringApplication.run(ProductProvider_Config_8003.class,args);
