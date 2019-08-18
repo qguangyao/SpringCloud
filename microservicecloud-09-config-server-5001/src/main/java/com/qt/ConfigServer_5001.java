f43feccfd709bfb52d99ec2795c1741d531344ac
qguangayo <1018399941@qq.com>
2019-08-19 00:13:30 +0800

    eureka config

--- microservicecloud-09-config-server-5001/src/main/java/com/qt/ConfigServer_5001.java
+++ microservicecloud-09-config-server-5001/src/main/java/com/qt/ConfigServer_5001.java
@@ -4,11 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.io.File;
+
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServer_5001 {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer_5001.class);
+        File directory = new File("");//设定为当前文件夹
+        try {
+            System.out.println(directory.getCanonicalPath());//获取标准的路径
+            System.out.println(directory.getAbsolutePath());//获取绝对路径
+        } catch (Exception e) {
+
+        }
     }
 }
---
+++ microservicecloud-10-consumer-product-8082/src/main/java/com/qt/ConfigClient_8082.java
@@ -0,0 +1,14 @@
+package com.qt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+
+@SpringBootApplication
+public class ConfigClient_8082 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_8082.class);
+    }
+}
